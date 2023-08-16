191ab13600433f95c1f433eca7cdcc92dbacbe57
Comatoki <hbabnd@gmail.com>
2023-08-16 18:46:01 +0900

    Initial Commit

---
+++ tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by COMATOKI on 2023-08-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
